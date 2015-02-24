cb0453afd268cacbe37a2a750752d480c11e203b
Jonah M. Duckles <jduckles@ou.edu>
2015-02-24 15:22:05 -0600

    helper script

--- mongo/config-server/run.sh
+++ mongo/config-server/run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-mkdir -p data/configdb
-docker run -d -v $(pwd)/data/configdb:/data/configdb mongo-config
+DB_STORAGE=$1
+LOCAL_PORT=$2
+
+if [ $# -lt 2 ]
+  then
+     echo "Please provide a fully qualified path and local port to accees this node from"
+     echo "  Example: ./run.sh $(pwd)/data 27017" 
+else
+    docker build -t mongo-config .
+    docker run -d -v ${DB_STORAGE}:/data/ -p $LOCAL_PORT:27017 mongo-config
+fi
