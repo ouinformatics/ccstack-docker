1d19d83b340549a671f28941cf2ce210886ba0de
Mark Stacy <mbstacy@gmail.com>
2015-03-02 15:14:53 -0600

    Added sleep to make sure ports are up and active prior to setup vhost,user,and password

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -5,11 +5,16 @@ if [ -z "$1" ]
     arg_host="localhost"
 fi
 echo $arg_host
-docker run -d -p 5672:5672 -p 15672:15672 dockerfile/rabbitmq
-#access 
+docker run -d -p 5672:5672 -p 15672:15672 cybercom/rabbitmq 
+
 source config.sh
-wget $arg_host:15672/cli/rabbitmqadmin
-chmod +x rabbitmqadmin
-./rabbitmqadmin -H $arg_host declare vhost name=$vhost
-./rabbitmqadmin -H $arg_host declare user name=$user password=$password tags=$tag
-./rabbitmqadmin -H $arg_host declare permission vhost=$vhost user=$user configure=".*" write=".*" read=".*"
+#sleep to allow for container ports to become active
+until wget $arg_host:15672/cli/rabbitmqadmin 
+do
+    sleep 5
+done
+
+chmod +x rabbitmqadmin && \
+./rabbitmqadmin -H $arg_host declare vhost name=$vhost && \
+./rabbitmqadmin -H $arg_host declare user name=$user password=$password tags=$tag && \
+./rabbitmqadmin -H $arg_host declare permission vhost=$vhost user=$user configure=".*" write=".*" read=".*" 
