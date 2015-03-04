8d9c36afbd03a8e9ffaa865ce0915746f4eadc13
Jonah Duckles <jduckles@ou.edu>
2015-03-04 14:15:02 -0600

    shebang change to bash and stronger variables {}

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Mark Stacy
 # arg_host added to accommodate boot2docker
@@ -12,17 +12,17 @@ if [ -z "$1" ]
   then
     arg_host="localhost"
 fi
-echo $arg_host
+echo ${arg_host}
 docker run -d -p 5672:5672 -p 15672:15672 cybercom/rabbitmq 
 
 source config.sh
 #sleep to allow for container ports to become active
-until wget $arg_host:15672/cli/rabbitmqadmin -O rabbitmqadmin 
+until wget ${arg_host}:15672/cli/rabbitmqadmin -O rabbitmqadmin 
 do
     sleep 5
 done
 
 chmod +x rabbitmqadmin && \
-./rabbitmqadmin -H $arg_host declare vhost name=$vhost && \
-./rabbitmqadmin -H $arg_host declare user name=$user password=$password tags=$tag && \
-./rabbitmqadmin -H $arg_host declare permission vhost=$vhost user=$user configure=".*" write=".*" read=".*" 
+./rabbitmqadmin -H ${arg_host} declare vhost name=${vhost} && \
+./rabbitmqadmin -H ${arg_host} declare user name=${user} password=${password} tags=${tag} && \
+./rabbitmqadmin -H ${arg_host} declare permission vhost=${vhost} user=${user} configure=".*" write=".*" read=".*" 
