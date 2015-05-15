8b318f026fa13ee9a16dc5ee088c43b99f051aac
Mark Stacy <mbstacy@gmail.com>
2015-05-15 15:52:59 -0500

    updated to run celery and rabbitmq. API is the only one left

--- run.sh
+++ run.sh
@@ -4,7 +4,15 @@
 docker run -d -v $(pwd)/mongo:/data -p 27017:27017 mongo-datanode
 
 ## RabbitMQ
-docker/rabbitmq/run.sh
+source $(pwd)/config/rabbitmq_config.sh
+docker run -d -p 5672:5672 -p 15672:15672 rabbitmq 
+sleep 5 #wait for container
+container=$(docker ps | grep rabbitmq | awk '{print $1}')
+docker exec -t $container rabbitmqctl add_user ${user} ${password}
+docker exec -t $container rabbitmqctl add_vhost ${vhost}
+docker exec -t $container rabbitmqctl set_permissions -p ${vhost} ${user} ".*" ".*" ".*"
+
 
 ## Celery
-docker run -d --net=host -e "C_FORCE_ROOT=true" -v celery/code:/code -v celery/log:/log -v celery/env:/env celery
+source $(pwd)/config/celery_config.sh
+docker run -d --net=host -e "C_FORCE_ROOT=true" -e "docker_worker=${docker_worker}" -e "docker_username=${docker_username}" -e "C_FORCE_ROOT=true" -e "CELERY_CONCURRENCY=${CELERY_CONCURRENCY}" -v celery/code:/code -v celery/log:/log -v celery/env:/env celery
